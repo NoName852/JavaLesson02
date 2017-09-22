7b4cf78d4bc0fc70e9699821a58285f0a7286448
NoName852 <19valeron94@gmail.com>
2017-09-22 19:17:04 +0300

    Линейные программы

---
+++ Lesson_000/Line01.java
@@ -0,0 +1,21 @@
+﻿/*
+Считать с клавиатуры число и строку и вывести их на экран
+*/
+import java.util.Scanner;
+
+class Line01 {
+    public static void main(String[] args) {
+        int a;
+        String stroka;
+        Scanner in = new Scanner(System.in); //Создание объекта для чтения с клавиатуры
+
+        System.out.print("Введи число: ");
+        a = in.nextInt();
+        in.nextLine();
+        System.out.print("Введи строку: ");
+        stroka = in.nextLine();
+
+        System.out.printf("Число = %d \n", a);
+        System.out.println("Строка = " + stroka);
+    }
+}
---
+++ Lesson_000/Line02.java
@@ -0,0 +1,23 @@
+﻿/*
+Рассчитать значение по формуле
+*/
+import java.util.Scanner;
+
+class Line02 {
+    public static void main(String[] args) {
+        int a, b, c;
+        double x;
+        Scanner in = new Scanner(System.in); //Создание объекта для чтения с клавиатуры
+        System.out.print("Введи a = ");
+        a = in.nextInt();
+        System.out.print("Введи b = ");
+        b = in.nextInt();
+        System.out.print("Введи c = ");
+        c = in.nextInt();
+        
+        x = a * ((Math.sin(b))/(20)) + Math.sqrt(c); //Расчет значения по формуле
+        System.out.print("Значение x = ");
+        System.out.println(x);
+        System.out.printf("Отформатированное значение x = %.4f", x);
+    }
+}
