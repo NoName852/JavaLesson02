0de60c0da731758dbbea7d89567d071f91f63898
NoName852 <19valeron94@gmail.com>
2017-09-29 11:37:32 +0400

    Обработка одномерных массивов

---
+++ Lesson_004/Array05.java
@@ -0,0 +1,44 @@
+﻿/*
+Дан произвольный массив целых двузначных чисел.
+Отсортировать массив по возрастанию методом выбора
+*/
+import java.util.Random;
+import java.util.Scanner;
+class Array05 {
+    public static void main(String[] args){
+        Random rnd = new Random();
+        Scanner scn = new Scanner(System.in);
+        int n;
+        int[] arrayA;
+        System.out.print("Количество чисел в массиве = ");
+        n = scn.nextInt();
+        arrayA = new int[n];
+        System.out.println("Исходный массив:");
+        //Заполняем массив случайными числами и выводим его на экран
+        for (int i = 0; i < n; i++) {
+            arrayA[i] = rnd.nextInt(99) + 1;
+            System.out.print(arrayA[i] + " ");
+        }
+        //Сортировка по возрастанию методом выбора
+        for (int i = 0; i < n; i++) {
+            int min = arrayA[i];
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arrayA[j] < min) {
+                    min = arrayA[j];
+                    minIndex = j;
+                }
+            }
+            if (i != minIndex) {
+                int temp = arrayA[i];
+                arrayA[i] = arrayA[minIndex];
+                arrayA[minIndex] = temp;
+            }
+        }
+        System.out.println("\nОтсортированный массив:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arrayA[i] + " ");
+        }
+
+    }
+}
---
+++ Lesson_004/Array06.java
@@ -0,0 +1,35 @@
+﻿/*
+Дан произвольный целочисленый двузначный одномерный массив.
+Отсортировать значения массива по убыванию методом пузырька
+*/
+import java.util.Scanner;
+import java.util.Random;
+class Array06 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        Random rnd = new Random();
+        int n;
+        System.out.print("Количество чисел в массиве = ");
+        n = scn.nextInt();
+        int[] array = new int[n];
+        //Заполнение массива и вывод на экран
+        for (int i = 0; i < n; i++) {
+            array[i] = rnd.nextInt(99) + 1;
+            System.out.print(array[i] + " ");
+        }
+        //Сортировка массива методом пузырька
+        for (int i = n - 1; i >= 2; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] < array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("\nОтсортированный массив:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
